d3b5e93e7d1c3580515f67be996e9392c5690cf4
RennAlt <renzprjkt@gmail.com>
2025-04-29 15:04:11 +0000

    Revert "fbdev: mdss: import MSM8953 changes"
    This reverts commit d1f3ebcbbedc0d1a7d0e45c1ae7d746147161abe.

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -350,7 +350,7 @@ static int mdss_dsi_regulator_init(struct platform_device *pdev,
 	return rc;
 }
 
-int mdss_dsi_panel_power_off(struct mdss_panel_data *pdata)
+static int mdss_dsi_panel_power_off(struct mdss_panel_data *pdata)
 {
 	int ret = 0;
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata = NULL;
